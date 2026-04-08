5534a6ca8f3141d995bed691e06c4fd3c97cc866
Kaio-dev2 <kaio_r_lisboa@estudante.sesisenai.org.br>
2026-04-08 09:47:33 -0300

    study: Adicionando anotações e implementações JPA nas classes User e Produto

--- fintrack/src/main/java/com/fintrack/fintrack/model/User.java
+++ fintrack/src/main/java/com/fintrack/fintrack/model/User.java
@@ -1,20 +1,33 @@
 package com.fintrack.fintrack.model;
 
-// Aqui temos a importação de Get e Set automáticos do Lombok
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;       //aqui importa o jakarta no caso o * importa tudo para os pacotes
+import lombok.Getter;               //Lombok Get
+import lombok.Setter;               //Lombok Set
 
-import java.math.BigDecimal;
 
 // Importei essa bíblioteca para ter o valor correto do saldo
-
-@Getter//declarei aqui o lembok get
-@Setter// e aqui o set
+import java.math.BigDecimal;
 
 
+// Aqui temos a importação de Get e Set automáticos do Lombok
+
+@Getter     //declarei aqui o lembok get
+@Setter     // e aqui o set
+
+@Entity         //ele fala: essa classe vira tabela no banco
+@Table(name = "usuarios") //ela fala: "o nome dessa tabela fica assim: "
 public class User {
 
+
+
+    @Id //Esse atributo é a chave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Esse GeneratedValue ele gera o ID automaticamente
+    private Long id;
+
+
+    //declarei a classe privada para nn ser atribuida em outro lugar
     private String nome;
     private String email;
     private String senha;
