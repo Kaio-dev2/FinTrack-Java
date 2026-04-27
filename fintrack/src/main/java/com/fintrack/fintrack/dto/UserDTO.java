28f8843f57027bce805bab837bdce32279e19e51
Kaio-dev2 <kaio_r_lisboa@estudante.sesisenai.org.br>
2026-04-27 16:35:46 -0300

    criando UserDTO com campos de cadastro

---
+++ fintrack/src/main/java/com/fintrack/fintrack/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.fintrack.fintrack.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class UserDTO {
+    private String nome;
+    private String email;
+    private String senha;
+
+
+}
