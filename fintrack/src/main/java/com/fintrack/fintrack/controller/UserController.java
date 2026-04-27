eae3d7e47ea4e1fd1b79b56d9a4c52ac9c145aae
Kaio-dev2 <kaio_r_lisboa@estudante.sesisenai.org.br>
2026-04-27 15:54:15 -0300

    study: cria UserController com injecao do UserService

---
+++ fintrack/src/main/java/com/fintrack/fintrack/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.fintrack.fintrack.controller;
+
+import  com.fintrack.fintrack.service.UserService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    public User salvar(User user){
+        return userService.salvar(user);
+    }
+}
